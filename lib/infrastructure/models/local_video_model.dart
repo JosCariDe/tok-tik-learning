df1e9e57dc719df4dcee1ee764c74ae1ddc7c0b2
joseLaptop <josenarvaezcariaga@gmail.com>
2025-07-02 20:53:20 -0500

    feat/convert de json a model creado

---
+++ lib/infrastructure/models/local_video_model.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok/domain/entities/video_post.dart';
+class LocalVideoModel {
+
+  final String name;
+  final String videoUrl;
+  final int likes;
+  final int views;
+
+  LocalVideoModel({
+    required this.name, 
+    required this.videoUrl, 
+    this.likes = 0, 
+    this.views = 0,
+  });
+
+  factory LocalVideoModel.fromJsonMap(Map<String, dynamic> json) => LocalVideoModel(
+    name: json["name"] ?? 'No name',
+    videoUrl: json["videoUrl"],
+    likes: json["likes"] ?? 0,
+    views: json["views"] ?? 0
+
+  );
+
+  Map<String, dynamic> toJson() => {
+    "name": name,
+    "videoUrl": videoUrl,
+    "likes": likes,
+    "views": views,
+  };
+
+  VideoPost toVIdeoPostEntity() => VideoPost(
+    caption: name, 
+    videoUrl: videoUrl,
+    likes: likes,
+    views: views 
+  );
+
+}
