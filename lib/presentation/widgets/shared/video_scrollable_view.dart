85d47047ab21c4475406391d4df659e739164028
joseLaptop <josenarvaezcariaga@gmail.com>
2025-07-03 21:06:52 -0500

    feat/empezando a crear los componentes En este caso se hizo el de btn (likes y vistas) y en este se posicionan en un stack

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:tiktok/domain/entities/video_post.dart';
+import 'package:tiktok/presentation/widgets/shared/video_buttons.dart';
 
 class VideoScrollableView extends StatelessWidget {
 
@@ -10,13 +11,28 @@ class VideoScrollableView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
       scrollDirection: Axis.vertical,
-      physics: const BouncingScrollPhysics(),
-      children: [
-        Container(color: Colors.red),
-        Container(color: Colors.lightBlue)
-      ],
+      physics: BouncingScrollPhysics(),
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+
+        final VideoPost videoPost = videos[index];
+
+        return Stack(
+          children: [
+            //VideoPlayer + Gradiente
+
+            //Botones de Likes
+            Positioned(
+              bottom: 40,
+              right: 20,
+              child: VideoButtons(video: videoPost)
+            )
+          ],
+        );
+
+      },
     );
   }
 }
