85a23e5ede43ea9869c409d87df4349680db890e
joseLaptop <josenarvaezcariaga@gmail.com>
2025-07-03 21:22:25 -0500

    feat/agregada animaciones a boton de play_circle

--- lib/presentation/widgets/shared/video_buttons.dart
+++ lib/presentation/widgets/shared/video_buttons.dart
@@ -1,6 +1,7 @@
 
 
- import 'package:flutter/material.dart';
+ import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
 import 'package:tiktok/config/helpers/human_formats.dart';
 import 'package:tiktok/domain/entities/video_post.dart';
 
@@ -15,7 +16,13 @@ import 'package:tiktok/domain/entities/video_post.dart';
     return Column(
       children: [
         _CustomIconButton(value: video.likes, iconData: Icons.favorite, iconColor: Colors.red),
-        _CustomIconButton(value: video.views, iconData: Icons.remove_red_eye_outlined)
+        const SizedBox(height: 20,),
+        _CustomIconButton(value: video.views, iconData: Icons.remove_red_eye_outlined),
+        const SizedBox(height: 20,),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration(seconds: 5),
+          child: const _CustomIconButton(value: 0, iconData: Icons.play_circle_outline))
       ],
     );
   }
@@ -38,6 +45,8 @@ class _CustomIconButton extends StatelessWidget {
     return Column(
       children: [
         IconButton(onPressed: () {}, icon: Icon(iconData, color: color, size: 30,)),
+
+        if(value > 0)
         Text(HumanFormats.humanReadbleNumber(value.toDouble()))
       ],
     );
